c52a2631f66629281016e7dcd73298fcc95af762
Ricardo Héctor Olivera <ricardohector.olivera@mercadolibre.com>
2023-02-13 14:03:40 -0300

    Modify Dependency injection on ItemController

--- src/main/java/com/example/controller/ItemController.java
+++ src/main/java/com/example/controller/ItemController.java
@@ -1,15 +1,18 @@
 package com.example.controller;
 
-
 import com.example.dto.item.ItemResponseDto;
 import com.example.service.impl.ItemService;
-import com.google.inject.Inject;
+import com.example.service.interfaces.IItemService;
 import io.javalin.http.Context;
 
 public class ItemController {
 
-    @Inject
-    private ItemService itemService;
+    private final IItemService itemService;
+
+    public ItemController() {
+        this.itemService = new ItemService();
+    }
+
 
     public void handleGetItemById(Context ctx) {
         String itemId = ctx.pathParam("itemId");
