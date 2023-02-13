df5582416eb7234ec77cdbcb991beb607906c4a0
Ricardo Héctor Olivera <ricardohector.olivera@mercadolibre.com>
2023-02-13 13:26:39 -0300

    Refactor dependency injection on ItemController.java

--- src/main/java/com/example/controller/ItemController.java
+++ src/main/java/com/example/controller/ItemController.java
@@ -2,7 +2,7 @@ package com.example.controller;
 
 
 import com.example.dto.item.ItemResponseDto;
-import com.example.service.impl.ItemService;
+import com.example.service.interfaces.IItemService;
 import io.javalin.http.Context;
 
 import javax.inject.Inject;
@@ -10,7 +10,7 @@ import javax.inject.Inject;
 public class ItemController {
 
     @Inject
-    private ItemService itemService;
+    private IItemService itemService;
 
     public void handleGetItemById(Context ctx) {
         String itemId = ctx.pathParam("itemId");
