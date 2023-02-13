dc544a56681f169b5ef460e8c81b6c37d1f1659c
Ricardo Héctor Olivera <ricardohector.olivera@mercadolibre.com>
2023-02-13 13:34:14 -0300

    Rollback dependency inversion on ItemController.java

--- src/main/java/com/example/controller/ItemController.java
+++ src/main/java/com/example/controller/ItemController.java
@@ -2,15 +2,14 @@ package com.example.controller;
 
 
 import com.example.dto.item.ItemResponseDto;
-import com.example.service.interfaces.IItemService;
+import com.example.service.impl.ItemService;
+import com.google.inject.Inject;
 import io.javalin.http.Context;
-
-import javax.inject.Inject;
 
 public class ItemController {
 
     @Inject
-    private IItemService itemService;
+    private ItemService itemService;
 
     public void handleGetItemById(Context ctx) {
         String itemId = ctx.pathParam("itemId");
