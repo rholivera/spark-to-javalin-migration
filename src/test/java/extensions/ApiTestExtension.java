c90b88b3354d041612e1ce6832f840f49e110197
Ricardo Héctor Olivera <ricardohector.olivera@mercadolibre.com>
2023-02-13 12:25:09 -0300

    Modify ApiTestExtension.java

--- src/test/java/extensions/ApiTestExtension.java
+++ src/test/java/extensions/ApiTestExtension.java
@@ -1,32 +1,34 @@
 package extensions;
 
-import com.example.router.Router;
+import com.example.WebServer;
+import com.google.common.net.MediaType;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
-import spark.Spark;
 
 @Slf4j
 public class ApiTestExtension implements BeforeAllCallback, AfterAllCallback {
+
+    private WebServer app;
 
     @Override
     public void beforeAll(final ExtensionContext extensionContext) throws Exception {
 
         //We put the Thread to sleep because in the integration test we need to wait for previous test server to shut down
         Thread.sleep(1000);
-        final int port = 8080;
-        Spark.port(port);
-        new Router().init();
-        Spark.awaitInitialization();
 
-        log.info("Spark server running in port: {} ...", port);
+        final int port = 8080;
+        app = new WebServer((config) -> config.defaultContentType = MediaType.JSON_UTF_8.toString());
+        app.start(port);
+
+        log.info("Javalin server running in port: {} ...", port);
 
     }
 
     @Override
     public void afterAll(final ExtensionContext extensionContext) {
-        Spark.stop();
-        log.info("Spark server stopped...");
+        log.info("Javalin server stopped...");
+        app.stop();
     }
 }
